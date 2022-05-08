3f2539a03c258638601ac6205e8bc32b9480469e
dengqing <dengqing0821@gmail.com>
2022-05-08 20:59:25 +0800

    fix: jsx ts error

---
+++ playground/src/jsx.d.ts
@@ -0,0 +1,9 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    card: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
+    'card-item': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
+    'card-item-label': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
+    'card-item-value': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
+
+  }
+}
