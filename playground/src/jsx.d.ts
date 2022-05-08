e97c54c4b157d50727d23354dfbf625705db864a
dengqing <dengqing0821@gmail.com>
2022-05-08 21:29:20 +0800

    fix: jsx error

--- playground/src/jsx.d.ts
+++ playground/src/jsx.d.ts
@@ -1,9 +1,13 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    card: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
-    'card-item': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
-    'card-item-label': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
-    'card-item-value': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
+export {}
 
+declare global {
+     namespace JSX {
+      interface IntrinsicElements {
+        card: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
+        'card-item': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
+        'card-item-label': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
+        'card-item-value': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
+    
+      }
+    }
   }
-}
