b5048299954c1e0d6b52eed460236d7f96c5a3cf
Dunqing <dengqing0821@gmail.com>
2022-06-05 08:39:17 +0000

    chore: use alias in vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
+import path from 'path'
+import { fileURLToPath } from 'url'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
+
+const dirname = path.dirname(fileURLToPath(import.meta.url))
+
+const resolve = (src: string) => {
+  return path.resolve(dirname, src)
+}
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@resumejs/components': resolve('./packages/components/src/index.ts'),
+    },
+  },
   plugins: [react()],
   test: {
     globals: true,
