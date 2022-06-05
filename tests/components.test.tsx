00bc8dbaa3660b62458aae1717f22d8c17797199
Dunqing <dengqing0821@gmail.com>
2022-06-05 08:17:53 +0000

    test: add test cases

--- tests/components.test.tsx
+++ tests/components.test.tsx
@@ -1,28 +1,64 @@
 import { describe, expect, it } from 'vitest'
 import { Resume } from '@resumejs/components'
 import { render } from '@testing-library/react'
-import React from 'react'
+
+const replaceWhitespace = (md: string) => {
+  return md.replace(/\n\s*/g, '\n')
+}
 
 describe('components', () => {
-  const md = `
-    # 张三
-
-    ![头像](https://notion-avatar.vercel.app/api/svg/eyJmYWNlIjo5LCJub3NlIjoxMCwibW91dGgiOjEsImV5ZXMiOjcsImV5ZWJyb3dzIjoxMSwiZ2xhc3NlcyI6MCwiaGFpciI6MTEsImFjY2Vzc29yaWVzIjoxMywiZGV0YWlscyI6MCwiYmVhcmQiOjAsImZsaXAiOjAsImNvbG9yIjoicmdiYSgyNTUsIDAsIDAsIDApIiwic2hhcGUiOiJub25lIn0=)
-
-    - [https://github.com/Dunqing](https://github.com/Dunqing)
-
-    >
-
-    - google@gmail.com
-    - 3 年
-
-    >
-
-    - Telegram: @luckingforme
-  `
   it('heading', () => {
+    const md = replaceWhitespace(`
+      # 张三
+
+      ![头像](https://notion-avatar.vercel.app/api/svg/eyJmYWNlIjo5LCJub3NlIjoxMCwibW91dGgiOjEsImV5ZXMiOjcsImV5ZWJyb3dzIjoxMSwiZ2xhc3NlcyI6MCwiaGFpciI6MTEsImFjY2Vzc29yaWVzIjoxMywiZGV0YWlscyI6MCwiYmVhcmQiOjAsImZsaXAiOjAsImNvbG9yIjoicmdiYSgyNTUsIDAsIDAsIDApIiwic2hhcGUiOiJub25lIn0=)
+
+      - [https://github.com/Dunqing](https://github.com/Dunqing)
+
+      >
+
+      - google@gmail.com
+      - 3 年
+
+      >
+
+      - Telegram: @luckingforme
+  `)
+    const { container } = render(<Resume>{md}</Resume>)
+    expect(container.innerHTML).toMatchSnapshot()
+  })
+
+  it('toolbox', () => {
+    const md = replaceWhitespace(`
+    ---
+    theme: true
+    print: true
+    github: 'https://github.com/Dunqing/resume'
+    ---
+  `)
+    const { container } = render(<Resume>{md}</Resume>)
+    expect(container.innerHTML).toMatchSnapshot()
+  })
+
+  it('card', () => {
+    const md = replaceWhitespace(`
+      ### 公司内部 Cli 开发（独立完成）
+
+      | 担任角色 |     项目周期      |
+      | :------: | :---------------: |
+      |  负责人  | 2020.11 ~ 2021.01 |
+    `)
     const { container } = render(<Resume components={{}}>{md}</Resume>)
-    console.log(container.innerHTML)
+    expect(container.innerHTML).toMatchSnapshot()
+  })
+
+  it('description', () => {
+    const md = replaceWhitespace(`
+      ### [swr-taro](https://www.npmjs.com/package/taro-swr)
+      
+      **业务背景是由于公司的小程序对需要对接口进行管理,(替换原来实现的 useRequest，api 不太友好)。考虑到内部的 H5 使用的是 SWR，统一请求管理库对维护者更加友好，所以决定把 SWR 适配到 Taro 中去**
+    `)
+    const { container } = render(<Resume components={{}}>{md}</Resume>)
     expect(container.innerHTML).toMatchSnapshot()
   })
 })
