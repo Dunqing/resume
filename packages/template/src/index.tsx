22c85427e20ec69c26d3327f504a847fdcc75749
Dunqing <dengqing0821@gmail.com>
2022-06-05 12:46:55 +0000

    feat: borders are not displayed during the printing phase

--- packages/template/src/index.tsx
+++ packages/template/src/index.tsx
@@ -32,7 +32,7 @@ export const generateComponents = ({
     content: ({ className, ...props }) => (
       <div
         className={clsx(
-          'border border-light-700 md:w-full md:max-w-screen-md xs:w-full mx-auto p-y-5 sm:p-x-10 p-x-4',
+          'border print:border-none border-light-700 md:w-full md:max-w-screen-md xs:w-full mx-auto p-y-5 sm:p-x-10 p-x-4',
           'dark:border-dark-100',
           'relative',
           className
