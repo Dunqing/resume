111f25b08e9502c0a8288eb180a125e07f31a323
dengqing <dengqing0821@gmail.com>
2022-06-25 22:24:32 +0800

    refactor: remove blockquote elements

--- packages/components/src/plugins/header.ts
+++ packages/components/src/plugins/header.ts
@@ -2,6 +2,7 @@ import type { Plugin } from 'unified'
 import { visit } from 'unist-util-visit'
 import type { Element } from 'hast'
 import { isBlockquote, isImage, isParagraph, isUl } from './_util'
+import { u } from 'unist-builder'
 
 export const header: Plugin<[], Element> = function () {
   return (root) => {
@@ -13,46 +14,53 @@ export const header: Plugin<[], Element> = function () {
       },
       (paragraph, index, parent) => {
         const len = parent!.children.length
-        const ul = []
+        const headerChildren: Element[] = []
         let avatar: Element
-        const delIndex: number[] = []
+
+        const replaceWithEmpty = (index: number) => {
+          parent!.children[index] = u('text', '')
+        }
+
 
         for (let i = index! + 2; i < len; i += 2) {
           const element = parent!.children[i] as Element
 
-          if (isBlockquote(element)) continue
+          if (isBlockquote(element)) {
+            replaceWithEmpty(i)
+            continue
+          }
 
           // avatar
           if (isParagraph(element) && isImage(element.children[0])) {
-            ;(element.children[0] as Element).tagName = 'header-avatar'
             avatar = element.children[0] as Element
-            delIndex.unshift(i)
+            replaceWithEmpty(i)
             continue
           }
 
           if (!isUl(element)) break
 
           element.tagName = 'header-row'
-          ul.push(element)
+          headerChildren.push(element)
           visit(element, { tagName: 'li' }, (li) => {
             li.tagName = 'header-col'
           })
 
-          delIndex.unshift(i)
+          replaceWithEmpty(i)
         }
 
-        if (avatar! || ul.length) {
-          if (avatar!) ul.unshift(avatar)
-          ul.unshift(paragraph)
-
-          paragraph.tagName = 'header-name'
-          delIndex.forEach((i) => parent!.children.splice(i, 1))
-          parent!.children.splice(index!, 1, {
-            type: 'element',
-            tagName: 'header',
-            children: ul,
-          })
+        if (avatar!) { 
+          avatar.tagName = 'header-avatar'
+          headerChildren.unshift(avatar)
         }
+
+        paragraph.tagName = 'header-name'
+        headerChildren.unshift(paragraph)
+
+        parent!.children.splice(index!, 1, {
+          type: 'element',
+          tagName: 'header',
+          children: headerChildren,
+        })
       }
     )
   }
