7985aa617771b14cc5903b798814e038ac9c9ddb
dengqing <dengqing0821@gmail.com>
2022-07-06 22:19:45 +0800

    feat(resume): support customize template

--- packages/resume/src/index.ts
+++ packages/resume/src/index.ts
@@ -13,7 +13,7 @@ interface ArgvOptions extends minimist.ParsedArgs {
   config?: string
 }
 
-const { _, config } = minimist(process.argv.slice(2), {
+const { _, config, template } = minimist(process.argv.slice(2), {
   string: '-',
 }) as ArgvOptions
 
@@ -27,7 +27,9 @@ const getViteConfig = () => {
   const _config: InlineConfig = {
     logLevel: 'info',
     configFile,
-    plugins: [react(), entry(), loadResume()],
+    plugins: [react(), entry({
+      template
+    }), loadResume()],
   }
 
   return _config
