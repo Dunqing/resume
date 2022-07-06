18075593d8573723f699a0d00b83677661b4c86f
dengqing <dengqing0821@gmail.com>
2022-07-06 22:05:13 +0800

    feat: improve implement

---
+++ templates/nova/src/toolbox/index.tsx
@@ -0,0 +1,36 @@
+import clsx from 'clsx'
+import { useTemplate } from '@resumejs/template'
+import { GithubLink } from './GithubButton'
+import { PrintButton } from './PrintButton'
+import { ThemeButton } from './ThemeButton'
+
+interface ToolboxProps {
+  meta: any
+  className?: string
+}
+
+const Toolbox: React.FC<ToolboxProps> = ({ meta, ...props }) => {
+  const { toggleTheme, print } = useTemplate()
+
+  return (
+    <div
+      {...props}
+      className={clsx(
+        'absolute right-4 top-2 flex gap-x-1.5 print:hidden',
+        props.className
+      )}
+    >
+      <GithubLink github={meta.github} />
+      <PrintButton
+        onClick={print}
+        print={meta.print}
+      />
+      <ThemeButton
+        onClick={() => toggleTheme}
+        theme={meta.theme}
+      />
+    </div>
+  )
+}
+
+export default Toolbox
