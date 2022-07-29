1f10ef8370275b11292f66622eea94cd78b69574
qing.deng <dengqing0821@gmail.com>
2022-07-29 11:10:57 +0800

    chore: fix `npm` and `yarn` cannot start

--- packages/resume/build.config.ts
+++ packages/resume/build.config.ts
@@ -4,4 +4,13 @@ export default defineBuildConfig({
   entries: ['src/index'],
   declaration: true,
   clean: true,
+  hooks: {
+    'rollup:options': (config, options) => {
+      ;[options.output].flat().forEach((out) => {
+        if (out) {
+          out.banner = '#!/usr/bin/env node'
+        }
+      })
+    },
+  },
 })
