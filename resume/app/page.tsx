324e63273ecadabc4bd34748205f7958400df3ef
Dunqing <dengqing0821@gmail.com>
2023-08-11 18:40:52 +0800

    feat: shadcn and tailwindcss initialized

--- resume/app/page.tsx
+++ resume/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
+        <Button>Hello Shadcn</Button>
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
